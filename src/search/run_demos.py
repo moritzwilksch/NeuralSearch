60118423a515f9d6fb876a0aeb65ed1bc9260c0a
Moritz Wilksch <moritzwilksch@gmail.com>
2022-03-25 10:55:46 +0100

    run demos pretty print

--- src/search/run_demos.py
+++ src/search/run_demos.py
@@ -1,4 +1,8 @@
-from lib2to3.pytree import Base
+#%%
+from rich.console import Console
+from rich.panel import Panel
+
+c = Console()
 
 import polars as pl
 
@@ -8,4 +12,9 @@ df = pl.read_parquet("data/ibkr_articles.parquet")
 model = TfidfSearchModel()
 model.fit(df)
 results = model.search("inactivity fee")
-print(results)
+
+#%%
+for url, title, body in results.rows():
+    c.print(Panel(f"{title}", style="bold"))
+    c.print(body, style="dim")
+    c.print()
